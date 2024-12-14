d2445d7b5c9552f84836f5a73954ad06ab72c5b0
Utku Akgüner <utkuakguner2010@hotmail.com>
2024-12-14 12:27:44 +0300

    remove package-lock.json

--- components/reusable/Center.tsx
+++ components/reusable/Center.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import classNames from 'classnames';
 
 interface CardProps {
     children?: React.ReactNode;
+    className?: string
+
 }
 
-const Center: React.FC<CardProps> = ({ children }) => {
+const Center: React.FC<CardProps> = ({ children, className }) => {
     return (
-        <div className="grow flex justify-center items-center">
+        <div className={classNames("grow flex justify-center items-center", className)}>
             {children}
         </div>
     );
